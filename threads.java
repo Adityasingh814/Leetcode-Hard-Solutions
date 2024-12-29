0ec77bab93b3294081b693d00dd4d60e59bab169
Aditya Singh <142660148+Adityasingh814@users.noreply.github.com>
2024-12-29 17:52:46 +0530

    commit

--- threads.java
+++ threads.java
@@ -1,32 +1,32 @@
+class countable{
+    int count;
+    public synchronized void increment(){
+        count++;
+    }
+}
 public class threads {
-    public static void main(String[] args){
+
+    public static void main(String[] args) throws InterruptedException{
+        countable c = new countable();
        Runnable obj1 = ()->{
        
-        for(int i = 0;i<5;i++){
-        System.out.println("in class A");
-        try {
-            Thread.sleep(10);
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+        for(int i = 0;i<1000;i++){
+          c.increment();
     }
         };
        Runnable obj2 = ()->{
-        for(int i = 0;i<5;i++){
-        System.out.println("in class B");
-        try {
-            Thread.sleep(10);
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+     
+        for(int i = 0;i<1000;i++){
+            c.increment();
     }
         };
         Thread t1 = new Thread(obj1);
         Thread t2=  new Thread(obj2);
   t1.start();
   t2.start();
+t1.join();
+t2.join();
+  System.out.println(c.count);
           
        }
     }
